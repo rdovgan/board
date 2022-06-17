4081e9453794573d5f5c6f8fcdbb930874768c9b
Roman Dovgan <romapsih@gmail.com>
2022-06-18 00:49:48 +0300

    Added rank system for cards

--- src/main/java/com/longboard/game/durak/card/CardRank.java
+++ src/main/java/com/longboard/game/durak/card/CardRank.java
@@ -2,16 +2,21 @@ package com.longboard.game.durak.card;
 
 public enum CardRank {
 
-	Six("6"), Seven("7"), Eight("8"), Nine("9"), Ten("10"), Jack("J"), Queen("Q"), King("K"), Ace("A");
+	Six("6", 1), Seven("7", 2), Eight("8", 3), Nine("9", 4), Ten("10", 5), Jack("J", 6), Queen("Q", 7), King("K", 8), Ace("A", 9);
 
 	String symbol;
+	Integer rank;
 
-	CardRank(String symbol) {
+	CardRank(String symbol, Integer rank) {
 		this.symbol = symbol;
+		this.rank = rank;
 	}
 
 	public void setSymbol(String symbol) {
 		this.symbol = symbol;
 	}
 
+	public void setRank(Integer rank) {
+		this.rank = rank;
+	}
 }
