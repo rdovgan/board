8262e140c469ab10db554818b14096147c808074
Roman Dovgan <romapsih@gmail.com>
2022-06-17 20:43:18 +0300

    Implemented playing card for game with Rank and Suit

--- src/main/java/com/longboard/game/durak/card/PlayingCard36.java
+++ src/main/java/com/longboard/game/durak/card/PlayingCard36.java
@@ -1,4 +1,20 @@
 package com.longboard.game.durak.card;
 
 public class PlayingCard36 {
+
+	private final CardSuit suit;
+	private final CardRank rank;
+
+	public PlayingCard36(CardSuit suit, CardRank rank) {
+		this.suit = suit;
+		this.rank = rank;
+	}
+
+	public CardSuit getSuit() {
+		return suit;
+	}
+
+	public CardRank getRank() {
+		return rank;
+	}
 }
