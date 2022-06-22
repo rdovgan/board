33442e16d6efbbe2250c6be4cfd42a13fb24ee2a
Roman Dovgan <romapsih@gmail.com>
2022-06-22 13:24:17 +0300

    Added IsCard implementation for PlayingCard36 class

--- src/main/java/com/longboard/game/durak/card/PlayingCard36.java
+++ src/main/java/com/longboard/game/durak/card/PlayingCard36.java
@@ -1,13 +1,27 @@
 package com.longboard.game.durak.card;
 
-public class PlayingCard36 {
+import com.longboard.base.CardType;
+import com.longboard.entity.IsPlayer;
+import com.longboard.entity.card.IsCard;
+import com.longboard.entity.card.IsCost;
 
+import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class PlayingCard36 implements IsCard {
+
+	private final UUID id = UUID.randomUUID();
 	private final CardSuit suit;
 	private final CardRank rank;
+	private final Long number;
+	private final String name;
 
 	public PlayingCard36(CardSuit suit, CardRank rank) {
 		this.suit = suit;
 		this.rank = rank;
+		this.number = (long) (suit.getValue() + rank.getValue());
+		this.name = suit.getSymbol() + rank.getSymbol();
 	}
 
 	public CardSuit getSuit() {
@@ -16,5 +30,55 @@ public class PlayingCard36 {
 
 	public CardRank getRank() {
 		return rank;
+	}
+
+	@Override
+	public UUID getId() {
+		return id;
+	}
+
+	@Override
+	public Long getNumber() {
+		return number;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getDescription() {
+		return name;
+	}
+
+	@Override
+	public CardType getCardType() {
+		return null;
+	}
+
+	@Override
+	public IsCost getCost() {
+		return null;
+	}
+
+	@Override
+	public Predicate<IsCard> getCondition() {
+		return null;
+	}
+
+	@Override
+	public void setOwner(IsPlayer<IsCard> player) {
+
+	}
+
+	@Override
+	public IsPlayer<IsCard> getOwner() {
+		return null;
+	}
+
+	@Override
+	public Consumer<IsCard> getEffect() {
+		return null;
 	}
 }
